cffa8234666e05f024a9d299533c3678fd7a00c7
k8tems <aiue@yahoo.co.jp>
2017-05-26 07:38:06 +0900

    Merge test cases

--- test.py
+++ test.py
@@ -10,35 +10,18 @@ class TestGetAreaSrc(unittest.TestCase):
         self.area['press'] = 10, 10
         self.area['release'] = 20, 20
         self.assertEqual((10, 10), self.area.src)
-
-    def test_press_y_greater_than_release_y(self):
-        self.area['press'] = 10, 30
-        self.area['release'] = 20, 20
-        self.assertEqual((10, 20), self.area.src)
-
-    def test_press_y_y_greater_than_release_y(self):
-        self.area['press'] = 20, 20
-        self.area['release'] = 10, 10
-        self.assertEqual((10, 10), self.area.src)
-
-
-class TestGetAreaDest(unittest.TestCase):
-    def setUp(self):
-        self.area = main.Area()
-
-    def test_press_x_y_less_than_release_x_y(self):
-        self.area['press'] = 10, 10
-        self.area['release'] = 20, 20
         self.assertEqual((20, 20), self.area.dest)
 
     def test_press_y_greater_than_release_y(self):
         self.area['press'] = 10, 30
         self.area['release'] = 20, 20
+        self.assertEqual((10, 20), self.area.src)
         self.assertEqual((20, 30), self.area.dest)
 
     def test_press_y_y_greater_than_release_y(self):
         self.area['press'] = 20, 20
         self.area['release'] = 10, 10
+        self.assertEqual((10, 10), self.area.src)
         self.assertEqual((20, 20), self.area.dest)
 
 
