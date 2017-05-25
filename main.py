201dd4dc581456493f77ab4c1f81456df364c114
k8tems <aiue@yahoo.co.jp>
2017-05-26 04:25:57 +0900

    Tweet

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import yaml
+from twython import Twython
+
+
+def get_api():
+    cfg = yaml.load(open('settings.yml', 'rb'))
+    return Twython(
+        cfg['consumer_key'],
+        cfg['consumer_secret'],
+        cfg['access_token_key'],
+        cfg['access_token_secret'])
+
+
 if __name__ == '__main__':
     print('Hello world!')
+    api = get_api()
+    api.update_status(status='test')
