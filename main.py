8040e8c6b0d383b76f0670d3e08d25950ee72c15
k8tems <aiue@yahoo.co.jp>
2017-05-26 11:24:33 +0900

    Add early check for settings

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import uuid
 import tempfile
 import yaml
 import time
+import tkMessageBox
 from twython import Twython
 from PIL import ImageGrab
 import area
 
 
+SETTINGS_FILE = 'settings.yml'
+
+
 def get_api():
-    cfg = yaml.load(open('settings.yml', 'rb'))
+    cfg = yaml.load(open(SETTINGS_FILE, 'rb'))
     return Twython(
         cfg['consumer_key'],
         cfg['consumer_secret'],
@@ -23,16 +27,30 @@ def share_image(image_file):
     api.update_status(media_ids=[media_id])
 
 
-if __name__ == '__main__':
+def show_error(msg):
+    tkMessageBox.showerror('Error', msg)
+
+
+def main():
+    if not os.path.exists(SETTINGS_FILE):
+        show_error('%s does not exist' % SETTINGS_FILE)
+        return
+
     a = area.get_area()
+
     if a.width == 0 or a.height == 0:
-        print('Invalid area ' + str(a))
-    else:
-        # wait until the window disappears
-        time.sleep(1)
-        image = ImageGrab.grab(a.bbox)
-        # can't get this to work with BytesIO
-        fname = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
-        image.save(fname, format='png')
-        with open(fname, 'rb') as f:
-            share_image(f)
+        show_error('Invalid area ' + str(a))
+        return
+
+    # wait until the window disappears
+    time.sleep(1)
+    image = ImageGrab.grab(a.bbox)
+    # can't get this to work with BytesIO
+    fname = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+    image.save(fname, format='png')
+    with open(fname, 'rb') as f:
+        share_image(f)
+
+
+if __name__ == '__main__':
+    main()
