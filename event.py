76a660fdce9e9b6a6d8c2cf67ab618437deb5872
k8tems <aiue@yahoo.co.jp>
2017-05-29 10:15:05 +0900

    Add comment

--- event.py
+++ event.py
@@ -2,4 +2,5 @@ MOUSE_MOVE = '<Motion>'
 LEFT_PRESS = '<Button-1>'
 LEFT_RELEASE = '<ButtonRelease-1>'
 RIGHT_PRESS = '<Button-3>'
+# custom events need to be surrounded by <<>>
 TWITTER_UPLOAD = '<<TwitterUpload>>'
