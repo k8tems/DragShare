38b208822fa7219f6ef75caa0f9d99cdf545f2f3
k8tems <aiue@yahoo.co.jp>
2017-05-29 11:10:19 +0900

    Isolate image file stuff

--- main.py
+++ main.py
@@ -30,11 +30,14 @@ def get_api(twitter_settings):
         cfg['access_token_secret'])
 
 
-def upload_image(image_file, twitter_settings):
+def upload_image(image, twitter_settings):
     """Upload image to dummy account"""
     api = get_api(twitter_settings)
     # It'd be better if I could just upload a BytesIO object but I can't get it to work
-    media_id = api.upload_media(media=image_file)['media_id']
+    image_file_name = generate_temp_file_name()
+    image.save(image_file_name, format='png')
+    with open(image_file_name, 'rb') as image_file:
+        media_id = api.upload_media(media=image_file)['media_id']
     resp = api.update_status(media_ids=[media_id])
     return resp['entities']['media'][0]['display_url']
 
@@ -70,19 +73,18 @@ def get_args():
     return parser.parse_args()
 
 
-def on_twitter_upload(image_file_name, twitter_settings, _):
+def on_twitter_upload(image_file, twitter_settings, _):
     logger.info('on_twitter_upload')
-    with open(image_file_name, 'rb') as f:
-        clipboard.copy(upload_image(f, twitter_settings))
+    clipboard.copy(upload_image(image_file, twitter_settings))
 
 
-def run_image_view(image_file_name, a, twitter_settings):
+def run_image_view(image, a, twitter_settings):
     image_view = Tk()
     # `PhotoImage` has to be instantiated after the root object and
     # also has to persist in a variable while the event loop is running
-    tkimage = ImageTk.PhotoImage(file=image_file_name)
+    tkimage = ImageTk.PhotoImage(image)
     view.setup_image_view(image_view, tkimage, a)
-    image_view.bind(event.TWITTER_UPLOAD, partial(on_twitter_upload, image_file_name, twitter_settings))
+    image_view.bind(event.TWITTER_UPLOAD, partial(on_twitter_upload, image, twitter_settings))
     image_view.mainloop()
 
 
@@ -107,9 +109,7 @@ def main():
         return
 
     image = take_screen_shot(a.bbox)
-    image_file_name = generate_temp_file_name()
-    image.save(image_file_name, format='png')
-    run_image_view(image_file_name, a, args.twitter_settings)
+    run_image_view(image, a, args.twitter_settings)
 
 
 if __name__ == '__main__':
