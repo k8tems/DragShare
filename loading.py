8e88729389a5ae72c8d3e24aad3a3447fcce0898
k8tems <aiue@yahoo.co.jp>
2017-06-06 22:50:10 +0900

    Show gif in center

--- loading.py
+++ loading.py
@@ -10,9 +10,10 @@ class LoadingAnimation(object):
         self.loading_frames = loading_frames
 
     def overlay(self, img):
-        enhanced_img = ImageEnhance.Brightness(img.copy()).enhance(3)
+        enhanced_img = ImageEnhance.Brightness(img.copy()).enhance(5)
         frame = self.loading_frames.next()
-        enhanced_img.paste(frame, (0, 0), frame)
+        loc = (enhanced_img.width/2-frame.width/2, enhanced_img.height/2-frame.height/2)
+        enhanced_img.paste(frame, loc, frame)
         return enhanced_img
 
 
@@ -24,7 +25,6 @@ def on_timer(ani, img, canvas):
 def dissect_gif(gif):
     result = []
     for i in range(gif.n_frames):
-        gif.copy().save('a/%d.png' % i)
         result.append(gif.copy().convert('RGBA'))
         gif.seek(i)
     return result
@@ -36,8 +36,7 @@ if __name__ == '__main__':
     img = img.convert('RGBA')
     root.geometry('%dx%d' % (img.width, img.height))
     canvas = view.ScreenshotCanvas(root, img)
-    gif = Image.open('loading2.gif')
-    frames = dissect_gif(gif)
+    frames = dissect_gif(Image.open('loading.gif'))
     ani = LoadingAnimation(cycle(frames))
     canvas.after(0, on_timer, ani, img, canvas)
     root.mainloop()
