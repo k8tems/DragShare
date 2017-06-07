6233ea6bd0ef66c697feb0f6c68898d460569519
k8tems <aiue@yahoo.co.jp>
2017-06-07 11:08:08 +0900

    Move test code

--- loading.py
+++ loading.py
@@ -1,8 +1,7 @@
-from tkinter import Tk
+
 from itertools import cycle
 from collections import namedtuple
 from PIL import Image
-import view
 
 
 class LoadingAnimation(object):
@@ -30,20 +29,3 @@ def dissect_gif(gif):
         gif.seek(i)
     return Gif(result, gif.info['duration'])
 
-
-def create_loading_animation(fname):
-    return LoadingAnimation(dissect_gif(Image.open(fname)))
-
-
-def on_timer(ani, img, canvas):
-    canvas.set_image(ani.overlay(img))
-    canvas.after(ani.delay, on_timer, ani, img, canvas)
-
-
-if __name__ == '__main__':
-    root = Tk()
-    img = Image.open('base.png')
-    root.geometry('%dx%d' % (img.width, img.height))
-    canvas = view.ScreenshotCanvas(root, img)
-    canvas.after(0, on_timer, create_loading_animation('loading.gif'), img, canvas)
-    root.mainloop()
