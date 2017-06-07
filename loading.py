8f37707a632fa516a802f91dd25be6a16d3081f3
k8tems <aiue@yahoo.co.jp>
2017-06-07 11:03:22 +0900

    Add test code and revamp loading animation

--- loading.py
+++ loading.py
@@ -1,6 +1,8 @@
+from tkinter import Tk
 from itertools import cycle
-from PIL import ImageEnhance, Image
 from collections import namedtuple
+from PIL import Image
+import view
 
 
 class LoadingAnimation(object):
@@ -9,11 +11,13 @@ class LoadingAnimation(object):
         self.loading_frames = cycle(gif.frames)
 
     def overlay(self, img):
-        enhanced_img = ImageEnhance.Brightness(img.copy()).enhance(4)
+        back_img = Image.new('RGBA', img.size, (255, 255, 255, 200))
         frame = self.loading_frames.next()
-        loc = (enhanced_img.width/2-frame.width/2, enhanced_img.height/2-frame.height/2)
-        enhanced_img.paste(frame, loc, frame)
-        return enhanced_img
+        loc = (back_img.width/2-frame.width/2, back_img.height/2-frame.height/2)
+        back_img.paste(frame, loc, frame)
+        base_img = img.copy()
+        base_img.paste(back_img, (0, 0), back_img)
+        return base_img
 
 
 Gif = namedtuple('Gif', 'frames delay')
@@ -29,3 +33,17 @@ def dissect_gif(gif):
 
 def create_loading_animation(fname):
     return LoadingAnimation(dissect_gif(Image.open(fname)))
+
+
+def on_timer(ani, img, canvas):
+    canvas.set_image(ani.overlay(img))
+    canvas.after(ani.delay, on_timer, ani, img, canvas)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    img = Image.open('base.png')
+    root.geometry('%dx%d' % (img.width, img.height))
+    canvas = view.ScreenshotCanvas(root, img)
+    canvas.after(0, on_timer, create_loading_animation('loading.gif'), img, canvas)
+    root.mainloop()
