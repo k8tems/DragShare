ba74e6ea150ce906af63272302a9d2afeaab8852
k8tems <aiue@yahoo.co.jp>
2017-06-08 12:29:03 +0900

    Add build script

---
+++ build.bat
@@ -0,0 +1,10 @@
+set APP_NAME=dragshare
+set RELEASE_DIR=dist\release
+set DEST_DIR=%RELEASE_DIR%\%APP_NAME%
+set LOADING_GIF=loading.gif
+set TWITTER_SETTINGS=settings\twitter.yml
+set LOGGING_SETTINGS=settings\logging\release.yml
+C:\Python27\Scripts\pyinstaller main.py -n %APP_NAME% --specpath build --noconsole --distpath %RELEASE_DIR%
+copy %LOADING_GIF% %DEST_DIR%\loading.gif
+copy %TWITTER_SETTINGS% %DEST_DIR%\twitter.yml
+copy %LOGGING_SETTINGS% %DEST_DIR%\log.yml
