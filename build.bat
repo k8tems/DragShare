6ff2aa4c3677e37fbaf5e37cefb25bfe8a1a7915
k8tems <aiue@yahoo.co.jp>
2017-06-08 12:32:30 +0900

    Edit paths

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 set PYINSTALLER=C:\Python27\Scripts\pyinstaller
 set APP_NAME=dragshare
-set RELEASE_DIR=dist\release
-set DEST_DIR=%RELEASE_DIR%\%APP_NAME%
+set DIST_DIR=dist\release
+set APP_DIR=%DIST_DIR%\%APP_NAME%
 set LOADING_GIF=loading.gif
 set TWITTER_SETTINGS=settings\twitter.yml
 set LOGGING_SETTINGS=settings\logging\release.yml
-%PYINSTALLER% main.py -n %APP_NAME% --specpath build --noconsole --distpath %RELEASE_DIR%
-copy %LOADING_GIF% %DEST_DIR%\loading.gif
-copy %TWITTER_SETTINGS% %DEST_DIR%\twitter.yml
-copy %LOGGING_SETTINGS% %DEST_DIR%\log.yml
+%PYINSTALLER% main.py -n %APP_NAME% --specpath build --noconsole --distpath %DIST_DIR%
+copy %LOADING_GIF% %APP_DIR%\loading.gif
+copy %TWITTER_SETTINGS% %APP_DIR%\twitter.yml
+copy %LOGGING_SETTINGS% %APP_DIR%\log.yml
