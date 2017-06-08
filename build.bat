4446fadc74d7ea580d1f41c2ef22396c179b3d50
k8tems <aiue@yahoo.co.jp>
2017-06-08 12:31:52 +0900

    Make pyinstaller path configurable

--- build.bat
+++ build.bat
@@ -1,10 +1,11 @@
+set PYINSTALLER=C:\Python27\Scripts\pyinstaller
 set APP_NAME=dragshare
 set RELEASE_DIR=dist\release
 set DEST_DIR=%RELEASE_DIR%\%APP_NAME%
 set LOADING_GIF=loading.gif
 set TWITTER_SETTINGS=settings\twitter.yml
 set LOGGING_SETTINGS=settings\logging\release.yml
-C:\Python27\Scripts\pyinstaller main.py -n %APP_NAME% --specpath build --noconsole --distpath %RELEASE_DIR%
+%PYINSTALLER% main.py -n %APP_NAME% --specpath build --noconsole --distpath %RELEASE_DIR%
 copy %LOADING_GIF% %DEST_DIR%\loading.gif
 copy %TWITTER_SETTINGS% %DEST_DIR%\twitter.yml
 copy %LOGGING_SETTINGS% %DEST_DIR%\log.yml
