09742c0484547c6c4725a2b589420d7a0f3b4f96
k8tems <aiue@yahoo.co.jp>
2017-06-08 12:36:36 +0900

    Undefine settings and pyinstaller

--- build.bat
+++ build.bat
@@ -1,10 +1,7 @@
-set PYINSTALLER=C:\Python27\Scripts\pyinstaller
 set APP_NAME=dragshare
 set DIST_DIR=dist\release
 set APP_DIR=%DIST_DIR%\%APP_NAME%
 set LOADING_GIF=loading.gif
-set TWITTER_SETTINGS=settings\twitter.yml
-set LOGGING_SETTINGS=settings\logging\release.yml
 %PYINSTALLER% main.py -n %APP_NAME% --specpath build --noconsole --distpath %DIST_DIR%
 copy %LOADING_GIF% %APP_DIR%\loading.gif
 copy %TWITTER_SETTINGS% %APP_DIR%\twitter.yml
